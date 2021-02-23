7bec83fa8836ef9c065cc8345b0fcb39bdd25c5e
Maksim Yurau <2004softwaredeveloper@gmail.com>
2021-02-23 20:34:28 +0300

    Project update. Updated BlueActivity.java.

--- app/src/main/java/com/example/simplecustomviews/BlueActivity.java
+++ app/src/main/java/com/example/simplecustomviews/BlueActivity.java
@@ -4,12 +4,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 
 public class BlueActivity extends Activity {
-    private final Paint paint = new Paint();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
